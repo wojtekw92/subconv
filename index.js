ceef5d18f9f5cf1a297f15522676cbfc082fc52a
wojtekw92 <wwozniak92@gmail.com>
2015-07-30 23:34:36 +0200

    subconv refactoring

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 var program = require('commander');
 var fs = require('fs'),
 Buffer = require('buffer').Buffer;
-//StringDecoder = require('string_decoder').StringDecoder;
-/*
-[
-  {
-    starttime:,
-    subs:
-  }
 
-]
-
-*/
 var checkType = function(data) {
-  if(data.match(/\d+:\d+:\d+:.*/i)) {
+  if(data.match(/^\d+:\d+:\d+:.*/i)) {
     return 'TMP';
-  } else if(data.match(/\{\d+\}\{\d+\}.*/i)) {
+  } else if(data.match(/^\{\d+\}\{\d+\}.*/i)) {
     return 'MPL2';
-  } else if(data.match(/\[\d+\]\[\d+\].*/i)) {
+  } else if(data.match(/^\[\d+\]\[\d+\].*/i)) {
     return 'MicroDVD';
-  } else if(data.match(/\d*\W*\n\d\d:\d\d:\d\d,\d*\s*-->\s*\d\d:\d\d:\d\d,\d*\W*\n/i)) {
+  } else if(data.match(/^\d*\W*\n\d\d:\d\d:\d\d,\d*\s*-->\s*\d\d:\d\d:\d\d,\d*\W*\n/i)) {
     return 'SubRip';
   } else {
     return 'unknow format';
   }
-}
+};
+
 var tmpTo = function(format, content, fps){
   switch(format) {
     case 'TMP':
@@ -43,7 +34,82 @@ var tmpTo = function(format, content, fps){
     case 'SubRip':
       break;
   }
-}
+};
+
+var mpl2To = function(format, content, fps) {
+  switch(format) {
+    case 'TMP':
+      content = content.replace(/\[(\d+)\]\[(\d+)\](.*)/gi,function(match,p1,p2,p3,offset,string) {
+        var time = ~~(parseInt(p1)/10);
+        var hours = ~~(time/3600);
+        time=time%3600;
+        var minutes = ~~(time/60);
+        var seconds =time%60;
+        return hours+':'+minutes+':'+seconds+':'+p3;
+      });
+      return content;
+      break;
+    case 'MPL2':
+      return content;
+      break;
+    case 'MicroDVD':
+      content = content.replace(/\{(\d+)\}\{(\d+)\}(.*)/gi,function(match,p1,p2,p3,offset,string) {
+        return '['+~~(parseInt(p1)*10/fps)+']'+'['+~~(parseInt(p2)*10/fps)+']'+p3;
+      });
+      return content;
+      break;
+    case 'SubRip':
+      break;
+  }
+};
+
+var microDvdTo = function(format, content, fps) {
+  switch(format) {
+    case 'TMP':
+      break;
+    case 'MPL2':
+      content = content.replace(/\[(\d+)\]\[(\d+)\](.*)/gi,function(match,p1,p2,p3,offset,string) {
+        return '{'+~~(parseInt(p1)*fps/10)+'}'+'{'+~~(parseInt(p2)*fps/10)+'}'+p3;
+      });
+      return content;
+      break;
+    case 'MicroDVD':
+      return content;
+      break;
+    case 'SubRip':
+      break;
+  }
+};
+
+var subRipTo = function(format, content, fps) {
+  switch(format) {
+    case 'TMP':
+      break;
+    case 'MPL2':
+      content = content.replace(/\d*\W*\n(\d\d):(\d\d):(\d\d),(\d*)\s*-->\s*(\d\d):(\d\d):(\d\d),(\d*)\W*\n/gi,function(match,p1,p2,p3,p4,p5,p6,p7,p8,offset,string) {
+        p1 = parseInt(p1)*3600;
+        p2 = parseInt(p2)*60;
+        p3 = parseInt(p3);
+        p4 = parseInt(p4)*0.001;
+
+        p5 = parseInt(p5)*3600;
+        p6 = parseInt(p6)*60;
+        p7 = parseInt(p7);
+        p8 = parseInt(p8)*0.001;
+
+        return '{'+~~((p1+p2+p3+p4)*fps)+'}'+'{'+~~((p5+p6+p7+p8)*fps)+'}';
+      });
+      content = content.replace(/\r\n/gi,'|');
+      content = content.replace(/\|\|/gi,'\r\n');
+      return content;
+      break;
+    case 'MicroDVD':
+      break;
+    case 'SubRip':
+      return content;
+      break;
+  }
+};
 var readFile = function(filename) {
   var data='';
   try {
@@ -88,14 +154,17 @@ program
     }
     var content = readFile(program.input);
     switch(checkType(content)) {
+      case 'TMP':
+          writeFile(program.output, tmpTo(program.convert, content,parseFloat(program.fps)));
+        break;
+      case 'MPL2':
+          writeFile(program.output, mpl2To(program.convert, content,parseFloat(program.fps)));
+        break;
       case 'MicroDVD':
-        if(program.convert="MPL2") {
-          content = content.replace(/\[(\d+)\]\[(\d+)\](.*)/gi,function(match,p1,p2,p3,offset,string) {
-            return '{'+~~(parseInt(p1)*parseFloat(program.fps)/10)+'}'+'{'+~~(parseInt(p2)*parseFloat(program.fps)/10)+'}'+p3;
-          });
-          writeFile(program.output, content);
-        }
+          writeFile(program.output, microDvdTo(program.convert, content,parseFloat(program.fps)));
+        break;
+      case 'SubRip':
+          writeFile(program.output, subRipTo(program.convert, content,parseFloat(program.fps)));
         break;
     }
-
   }
