7ffdfbb78a001d83d76e8b915f1d954701716cd9
wojtekw92 <wwozniak92@gmail.com>
2015-09-30 22:57:57 +0200

    small refactoring

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ var tmpTo = function(format, content, fps){
       break;
     case 'MPL2':
       var subsArray=[];
-      content.replace(/(\d+):(\d+):(\d+):(.*)/gi,function(match,p1,p2,p3,p4,offset,string) {
+      content.replace(/(\d+):(\d+):(\d+):(.*)/gi,
+              function(match,p1,p2,p3,p4,offset,string) {
         var beginTime =(parseInt(p1)*3600+parseInt(p2)*60+parseInt(p3))*10;
         subsArray.push({startTime:beginTime,sub:p4});
       });
@@ -40,7 +41,8 @@ var tmpTo = function(format, content, fps){
 var mpl2To = function(format, content, fps) {
   switch(format) {
     case 'TMP':
-      content = content.replace(/\[(\d+)\]\[(\d+)\](.*)/gi,function(match,p1,p2,p3,offset,string) {
+      content = content.replace(/\[(\d+)\]\[(\d+)\](.*)/gi,
+                        function(match,p1,p2,p3,offset,string) {
         var time = ~~(parseInt(p1)/10);
         var hours = ~~(time/3600);
         time=time%3600;
@@ -54,7 +56,8 @@ var mpl2To = function(format, content, fps) {
       return content;
       break;
     case 'MicroDVD':
-      content = content.replace(/\{(\d+)\}\{(\d+)\}(.*)/gi,function(match,p1,p2,p3,offset,string) {
+      content = content.replace(/\{(\d+)\}\{(\d+)\}(.*)/gi,
+                        function(match,p1,p2,p3,offset,string) {
         return '['+~~(parseInt(p1)*10/fps)+']'+'['+~~(parseInt(p2)*10/fps)+']'+p3;
       });
       return content;
@@ -69,7 +72,8 @@ var microDvdTo = function(format, content, fps) {
     case 'TMP':
       break;
     case 'MPL2':
-      content = content.replace(/\[(\d+)\]\[(\d+)\](.*)/gi,function(match,p1,p2,p3,offset,string) {
+      content = content.replace(/\[(\d+)\]\[(\d+)\](.*)/gi,
+                        function(match,p1,p2,p3,offset,string) {
         return '{'+~~(parseInt(p1)*fps/10)+'}'+'{'+~~(parseInt(p2)*fps/10)+'}'+p3;
       });
       return content;
@@ -87,7 +91,8 @@ var subRipTo = function(format, content, fps) {
     case 'TMP':
       break;
     case 'MPL2':
-      content = content.replace(/\d*\W*\n(\d\d):(\d\d):(\d\d),(\d*)\s*-->\s*(\d\d):(\d\d):(\d\d),(\d*)\W*\n/gi,function(match,p1,p2,p3,p4,p5,p6,p7,p8,offset,string) {
+      content = content.replace(/\d*\W*\n(\d\d):(\d\d):(\d\d),(\d*)\s*-->\s*(\d\d):(\d\d):(\d\d),(\d*)\W*\n/gi,
+                        function(match,p1,p2,p3,p4,p5,p6,p7,p8,offset,string) {
         p1 = parseInt(p1)*3600;
         p2 = parseInt(p2)*60;
         p3 = parseInt(p3);
@@ -133,7 +138,8 @@ var writeFile = function(filename, data) {
 }
 program
   .version('0.0.1')
-  .option('-c, --convert <format>', 'convert input subtitles to output with selected format')
+  .option('-c, --convert <format>', 'convert input subtitles to output with'+
+                                                          ' selected format')
   .option('-i, --input <file>', 'input file')
   .option('-o, --output <file>', 'output file')
   .option('-C, --check <file>', 'Chceck subtitle format in file')
@@ -160,16 +166,27 @@ program
     }
     switch(checkType(content)) {
       case 'TMP':
-          writeFile(output, tmpTo(program.convert, content,parseFloat(program.fps)));
+          writeFile(output, tmpTo(program.convert,
+                                  content,
+                                  parseFloat(program.fps)));
         break;
       case 'MPL2':
-          writeFile(output, mpl2To(program.convert, content,parseFloat(program.fps)));
+          writeFile(output, mpl2To(program.convert,
+                                   content,
+                                   parseFloat(program.fps)));
         break;
       case 'MicroDVD':
-          writeFile(output, microDvdTo(program.convert, content,parseFloat(program.fps)));
+          writeFile(output, microDvdTo(program.convert,
+                                       content,
+                                       parseFloat(program.fps)));
         break;
       case 'SubRip':
-          writeFile(output, subRipTo(program.convert, content,parseFloat(program.fps)));
+          writeFile(output, subRipTo(program.convert,
+                                     content,
+                                     parseFloat(program.fps)));
+        break;
+      default:
+          console.log("Unsuported format!");
         break;
     }
   }
